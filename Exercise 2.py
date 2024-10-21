d5c19f3e000e106bc9d69bd01fd962e7e7336436
ALThea1010 <164994153+ALThea1010@users.noreply.github.com>
2024-10-21 11:33:51 +0400

    Add files via upload

---
+++ Exercise 2.py
@@ -0,0 +1,13 @@
+"""
+Assessment1-Programming-Skills-Portfolio Exercise 2: Simple Sums 
+"""
+
+# Declare variables and input with values 8 and 10
+num1 = 8
+num2 = 10
+
+# Calculate the sum of the first two numbers
+result = num1 + num2
+
+# Print the sum to the console
+print("SUM =",result)
